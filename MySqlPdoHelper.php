f090ace377ac52b51935b4fb9739a99e90e67c88
David Hoffman <David Hoffman@MMXCA>
2014-09-23 16:21:52 -0400

    Initial setup

---
+++ MySqlPdoHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace mmxca\MySqlPdoHelper;
+
+/**
+ * Description of MySqlPdoHelper
+ *
+ * @author David Hoffman
+ */
+class MySqlPdoHelper {
+    //put your code here
+}
